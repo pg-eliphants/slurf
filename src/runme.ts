cfa8453e7dc4ca1ad3baa1ba8b45cfdb042aa522
Jacob Bogers <jkfbogers@gmail.com>
2023-07-16 22:14:16 +0200

    pg-protocol added + stuff from mono repo parent dir

---
+++ src/runme.ts
@@ -0,0 +1,11 @@
+import PG from "pg";
+console.log(Object.keys(PG));
+const client = new PG.Client({ user: "postgres", password: "postgres" });
+await client.connect();
+for (let i = 0; i < 500; i++) {
+  const res = await client.query("SELECT $1::text as message, CURRENT_TIME", [
+    "Hello world!",
+  ]);
+  console.log(res.rows); // Hello world!
+}
+await client.end();
