e7e6499eee26e34f24ddbab930ec04289eb0c109
jacobbogers <jkfbogers@gmail.com>
2024-02-05 11:16:32 +0100

    createDeferOnce, to defer operation to next microcycle

---
+++ packages/sockets-and-streams/src/utils/deferOnce.ts
@@ -0,0 +1,32 @@
+export function createDeferOnce() {
+    let hitCountBeforeExecution = 0;
+    let hitCountDuringExecution = 0;
+    let executing = false;
+
+    return async function defer<T extends Function>(fn: T) {
+        if (executing) {
+            hitCountDuringExecution++;
+            hitCountBeforeExecution = 0;
+            return;
+        }
+        if (hitCountBeforeExecution) {
+            hitCountBeforeExecution++;
+            return;
+        }
+        // no recursion to avoid running out of stack space (in case of "infinite" (to deep) recursion)
+        do {
+            hitCountBeforeExecution = 0;
+            hitCountDuringExecution = 0;
+            executing = true;
+            await Promise.resolve()
+                .then(async () => {
+                    return fn();
+                })
+                .catch(() => {
+                    // sink errors
+                });
+            executing = false;
+        } while (hitCountDuringExecution);
+        hitCountBeforeExecution = 0;
+    };
+}
